73be777706c86efbb46fc3267f7d2e3a45e55f5f
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-04-07 13:34:26 +0200

    feat: add javascript module that loads about page content

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import html from "./index.html";
 import './style.css';
 import { initPageLoad } from './home.js';
 import { menuPage } from './menu.js';
+import { aboutPage } from './about.js';
 
 initPageLoad();
 menuPage();
+aboutPage();
