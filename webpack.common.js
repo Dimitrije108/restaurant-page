3b1026e9e4c2794a74cace7175c68f2b2cc74150
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-04-08 15:50:06 +0200

    chore: update webpack config so both start and build scripts work properly

--- webpack.common.js
+++ webpack.common.js
@@ -8,6 +8,26 @@ module.exports = {
         template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+       type: 'asset/resource',
+      },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
